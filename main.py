bb8b0914b0b371393f6f97f4cbb03e457e8708d8
Mateus Iketani <65353228+IketaniPG@users.noreply.github.com>
2020-08-26 19:02:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ ydl_opts = {}
 
 def main():
     arg = sys.argv[1]
+    
     try:
         os.chdir(directory)
         print(f'Foi detectado que a pasta {directory} já existe e o arquivo está sendo baixado:')
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([f'{arg}'])
+            
     except OSError:
         os.mkdir(directory)
         print(f'A pasta {directory} não existia e foi criada pelo Script, o arquivo está sendo baixado:')
         os.chdir(directory)
+        
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             ydl.download([f'{arg}'])
 try:
     main()
+    
 except IndexError:
     print('Não foi detectado nenhum URL!')
